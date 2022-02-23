cc09825f3ca0bace5ff4c1b94915775c29e72fd0
Scott Green <sagreenxyz@gmail.com>
2022-02-22 23:08:29 -0600

    Adding route for the 404 error

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.get('*', (req, res) => {
+    res.send('Nice page, not an ugly error');
+})
+
 app.get('/search', (req, res) => {
     let item = req.query.term;
     res.send('You searched for: ', item);
